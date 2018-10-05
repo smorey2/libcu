a2d80c0a8c4d2fea55323bc102305861dfa2d9cc
Sky Morey <smorey@degdigital.com>
2018-10-05 14:11:40 -0500

    removed CLR

--- src/libcu.tests/stdafx.h
+++ src/libcu.tests/stdafx.h
@@ -6,26 +6,4 @@
 
 #include <cuda_runtimecu.h>
 
-void allClassInitialize(bool sentinel = true);
-void allClassCleanup(bool sentinel = true);
-void allTestInitialize();
-void allTestCleanup();
-
 void mainPause(char *message = nullptr);
-
-#define __BEGIN_TEST(name, testInitialize, testCleanup) \
-	[TestClass] \
-	public ref class name##Test { \
-private: \
-	TestContext^ _testCtx; \
-public: \
-	[ClassInitialize()] static void ClassInitialize(Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ testContext) { allClassInitialize(); } \
-	[ClassCleanup()] static void ClassCleanup() { allClassCleanup(); } \
-	[TestInitialize()]void TestInitialize() { allTestInitialize(); testInitialize } \
-	[TestCleanup()] void TestCleanup() { allTestCleanup(); testCleanup } \
-	property Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ TestContext { \
-		Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ get() { return _testCtx; } \
-		System::Void set(Microsoft::VisualStudio::TestTools::UnitTesting::TestContext^ value) { _testCtx = value; } \
-}
-
-#define __END_TEST };
